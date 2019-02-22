3047e74a7cbd5d6e02db05446bb28895c4e4c388
Daniel King <drkwkd@mail.missouri.edu>
2019-02-21 20:57:20 -0600

    Added some profile component things

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.less']
 })
 export class ProfileComponent implements OnInit {
+  userData = { username: '', password: '', firstName: '', lastName: '', height: '', gender: '', dob: '', bodyFat: '', weight: '' };
+  username = 'Applebottom Jean';
+  canEdit = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  allowEditing() {
+    this.canEdit = !this.canEdit;
   }
 
 }
